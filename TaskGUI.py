043d7443f506044398251be20a89e87f96eb618b
JZ0024 <jessica.zhang123@gmail.com>
2019-04-13 18:51:44 -0400

    renamed TaskGUI_Tree into TaskGUI and renamed TaskGUI (original one) into TaskGUI_v2

--- TaskGUI.py
+++ TaskGUI.py
@@ -10,27 +10,43 @@ layout = [
             #do_not_clear=False clears the input box everytime event is True
             [sg.Text('Priority: ', size=(15,1)), sg.InputText(do_not_clear=False)],
             [sg.Button('Add'), sg.Button('Done'), sg.Button('Clear')],
-            [sg.Output(size=(80,10))]
         ]
 
-window = sg.Window('Task Organizer App').Layout(layout)
+mainWindow = sg.Window('Task Organizer App').Layout(layout)
+treeWindow_active = False
 root = Node(None, None)
 
-#dict = {'LOW': 1, 'MEDIUM': 2, 'HIGH': 3}
+treeData = sg.TreeData()
+
+def insertTree(root):
+    if root.left:
+        insertTree(root.left)
+    treeData.Insert("", root.task, root.task, root.data)      
+    if root.right:
+        insertTree(root.right)
 
 while True:
-    event, values = window.Read()
+    event, values = mainWindow.Read()
     if event is None:
+        mainWindow.Close()
         break   
     elif event == 'Add':
-        print(values[0], values[1])
         root.insert(values[0], int(values[1]))
-    elif event == 'Done':
-        print('SIZE:')
-        print(root.getSize())
-        print('TREE:')
-        root.PrintTree()
-    #elif event == 'Clear': #clear the console print screen
-        
+    elif event == 'Done' and not treeWindow_active:
+        treeWindow_active = True
+        mainWindow.Hide()
+        insertTree(root)
+        layout2 = [[sg.Text('Tasks')], [sg.Tree(data = treeData, headings=['PRIORITY'],change_submits=True, auto_size_columns=True, key='_TREE_', show_expanded=True)]]
 
-window.Close()
+        treeWindow = sg.Window('Result').Layout(layout2)
+
+        while True:
+            event2, values2 = treeWindow.Read()
+            
+            if event is None:
+                treeWindow.Close()
+                treeWindow_active = False
+                mainWindow.UnHide()
+                break
+            
+
