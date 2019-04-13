9f2adde2dc5c0ed8296c165a265a5c398296b1db
joysxara <joysxara@gmail.com>
2019-04-13 16:13:13 -0400

    updated GUI and backup GUI

--- TaskGUI.py
+++ TaskGUI.py
@@ -1,32 +1,36 @@
 import PySimpleGUI as sg
+import sys
 from binaryTree import Node
 
 # GUI for Task Organizer
 
 layout = [
             [sg.Text('Task Organizer')],
-            [sg.Text('Task Name: ', size=(15,1)), sg.InputText()],
-            [sg.Text('Priority: ', size=(15,1)), sg.InputCombo(['LOW', 'MEDIUM', 'HIGH'], size=(20,3))],
-            [sg.Button('Add'), sg.Button('Done')],
+            [sg.Text('Task Name: ', size=(15,1)), sg.InputText(do_not_clear=False)],
+            #do_not_clear=False clears the input box everytime event is True
+            [sg.Text('Priority: ', size=(15,1)), sg.InputText(do_not_clear=False)],
+            [sg.Button('Add'), sg.Button('Done'), sg.Button('Clear')],
             [sg.Output(size=(80,10))]
         ]
 
 window = sg.Window('Task Organizer App').Layout(layout)
 root = Node(None, None)
 
-dict = {'LOW': 1, 'MEDIUM': 2, 'HIGH': 3}
+#dict = {'LOW': 1, 'MEDIUM': 2, 'HIGH': 3}
 
 while True:
     event, values = window.Read()
     if event is None:
         break   
     elif event == 'Add':
-        print(values[0], dict[values[1]])
-        root.insert(values[0], dict[values[1]])
+        print(values[0], values[1])
+        root.insert(values[0], int(values[1]))
     elif event == 'Done':
         print('SIZE:')
         print(root.getSize())
         print('TREE:')
         root.PrintTree()
+    #elif event == 'Clear': #clear the console print screen
+        
 
 window.Close()
