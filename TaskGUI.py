d2ba75158dd418449443cc7a25a8d8c5d9078a7d
joysxara@gmail.com <joysxara@gmail.com>
2019-04-13 19:48:02 -0400

    please update the TaskGUI

--- TaskGUI.py
+++ TaskGUI.py
@@ -1,20 +1,20 @@
 import PySimpleGUI as sg
-import sys
 from binaryTree import Node
 
 # GUI for Task Organizer
 
 layout = [
             [sg.Text('Task Organizer')],
-            [sg.Text('Task Name: ', size=(15,1)), sg.InputText(do_not_clear=False)],
+            [sg.Text('Task Name: ', size=(18,1)), sg.InputText(do_not_clear=False)],
             #do_not_clear=False clears the input box everytime event is True
-            [sg.Text('Priority: ', size=(15,1)), sg.InputText(do_not_clear=False)],
+            [sg.Text('Priority: ', size=(18,1)), sg.InputText(do_not_clear=False)],
+            [sg.Text('Due Date (YYYYMMDD): ', size=(18,1)), sg.InputText(do_not_clear=False)],
             [sg.Button('Add'), sg.Button('Done'), sg.Button('Clear')],
         ]
 
 mainWindow = sg.Window('Task Organizer App').Layout(layout)
 treeWindow_active = False
-root = Node(None, None)
+root = Node(None, 0, 0)
 
 treeData = sg.TreeData()
 
@@ -31,7 +31,7 @@ while True:
         mainWindow.Close()
         break   
     elif event == 'Add':
-        root.insert(values[0], int(values[1]))
+        root.insert(values[0], int(values[1]), int(values[2]))
     elif event == 'Done' and not treeWindow_active:
         treeWindow_active = True
         mainWindow.Hide()
